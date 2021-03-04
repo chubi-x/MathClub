9a2e57e0d1c5d1f251f02522b26dccba55968c07
chubi-x <michaeladejoh01@gmail.com>
2021-03-04 16:17:15 +0400

    added pwa capabilities

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+var cacheName = 'mathclubv1';
+var cacheFiles = [
+    'math.html',
+    'math.jpg',
+    'mathclub.webmanifest'
+];
+self.addEventListener('install',(e)=>{
+    console.log('[Service Worker] install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache)=>{
+            console.log('[Service Worker] caching all files');
+            return cache.addAll(cacheFiles);
+        })
+    )
+});
+self.addEventListener('fetch',(e)=>{
+    e.respondWith(
+        caches.match(e.request).then(
+            (r)=>{
+                //download file if it is not in the cache
+                return r ||fetch(e.request).then((response)=>{
+                    //add new files to the cache
+                    return caches.open(cacheName).then((cache)=>{
+                        cache.put(e.request,response.clone());
+                        return response;        
+                    })
+                })
+            }
+        )
+    )
+})
